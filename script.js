0a4ef467681a80495cd2ab4d83a5095b4fbdcfef
Mike Ruane <mikeruanedev@gmail.com>
2021-01-30 13:38:52 -0500

    table test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let searchBoxEl;
 let searchBtnEl;
 let resultsTableEl;
 let resultsTableElData;
+let playlistResults = [];
 const apiKey = "AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0";
 const ytMoviesChannelId = "UClgRkhTL3_hImCAmdLfDE4g";
 
@@ -11,10 +12,24 @@ window.onload = function() {
     searchBtnEl = document.querySelector("#searchBtn");
     resultsTableEl = document.querySelector("#resultsTable");
     resultsTableDataEl = document.querySelector("#resultsTableData");
+
+    
+    searchBtnEl.addEventListener("click", function() {
+        //get yt movies playlist from api
+        //https://www.googleapis.com/youtube/v3/search?key=AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0&channelId=UClgRkhTL3_hImCAmdLfDE4g&part=snippet,id&order=date&maxResults=30
+        fetch("https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&channelId=" + ytMoviesChannelId + "&part=snippet,id&order=date&maxResults=30")
+        .then(function(response) {
+            playlistResults = response.json();
+        })
+
+        for (let i = 0; i < playlistResults.length; i++) {
+            let tableRow = "<tr>" +
+            "<td>" + playlistResults.items.snippet.title + "</td>" + 
+            "<td>" + playlistResults.items.snippet.publishTime + "</td></tr>";
+            resultsTableDataEl.appendChild(tableRow);     
+        }
+
+    });
+
 }
-
-searchBtnEl.addEventListener("click", function() {
-    //get yt movies playlist from api 
-});
-
 
