0293905764c04ef1ffb5814b3dced09918ba1d15
Mike Ruane <mikeruanedev@gmail.com>
2021-01-30 13:06:01 -0500

    initial markup

---
+++ script.js
@@ -0,0 +1,20 @@
+let searchBoxEl;
+let searchBtnEl;
+let resultsTableEl;
+let resultsTableElData;
+const apiKey = "AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0";
+const ytMoviesChannelId = "UClgRkhTL3_hImCAmdLfDE4g";
+
+window.onload = function() {
+
+    searchBoxEl = document.querySelector("#searchBox");
+    searchBtnEl = document.querySelector("#searchBtn");
+    resultsTableEl = document.querySelector("#resultsTable");
+    resultsTableDataEl = document.querySelector("#resultsTableData");
+}
+
+searchBtnEl.addEventListener("click", function() {
+    //get yt movies playlist from api 
+});
+
+
