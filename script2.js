1933dbc9530d22d7a4ac8fd1d55ba1287110ca2d
chrisPicano <cpicano25@gmail.com>
2021-02-09 20:09:08 -0500

    Updated background

--- script2.js
+++ script2.js
@@ -6,7 +6,7 @@ let movieClipsBtn
 const apiKey = "AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0";
 const ytMoviesChannelId = "UClgRkhTL3_hImCAmdLfDE4g";
 
-window.onload = function() {
+window.onload = function () {
 
     searchBoxEl = document.querySelector("#searchInput");
     searchBtnEl = document.querySelector("#searchBtn");
@@ -17,31 +17,32 @@ window.onload = function() {
     //https://www.googleapis.com/youtube/v3/search?type=video&videoType=movie&relevanceLanguage=en&part=snippet&order=viewCount&maxResults=20&key=AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0
     //https://www.googleapis.com/youtube/v3/search?type=video&q=jurassic%20park&relevanceLanguage=en&part=snippet&order=viewCount&maxResults=20&key=AIzaSyBnfgMuKMAFU1nzcXQCZY2xWmbuvr7gYI0
 
-    searchBtnEl.addEventListener("click", function() {
+    searchBtnEl.addEventListener("click", function () {
         //get movies from search
         let searchText = searchBoxEl.value;
         alert(searchText);
         fetch("https://www.googleapis.com/youtube/v3/search?type=video&q=" + searchText + "&relevanceLanguage=en&part=snippet&maxResults=20&key=" + apiKey)
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
-        })
-        .then(function(data) {
-            for (let i = 0; i < 5; i++) {
-                //get thumbnail elements            
-                let blockTitleEl = document.querySelector("#resultBlockTitle" + i);
-                let videoIconEl = document.querySelector("#videoIcon" + i);
-                let resultBlockCaptionEl = document.querySelector("#resultBlockCaption" + i);
+            })
+            .then(function (data) {
+                for (let i = 0; i < 5; i++) {
+                    //get thumbnail elements            
+                    let blockTitleEl = document.querySelector("#resultBlockTitle" + i);
+                    let videoIconEl = document.querySelector("#videoIcon" + i);
+                    let resultBlockCaptionEl = document.querySelector("#resultBlockCaption" + i);
+                    console.log(data)
 
-                //set content fron result
-                let item = data.items[i];                
-                videoIconEl.setAttribute("src", item.snippet.thumbnails.default.url);
-                resultBlockCaptionEl = item.snippet.publishTime;
+                    //set content fron result
+                    let item = data.items[i];
+                    videoIconEl.setAttribute("src", item.snippet.thumbnails.default.url);
+                    resultBlockCaptionEl = item.snippet.publishTime;
 
-                if (item.snippet.title != null || item.snippet.title != undefined) {
-                    blockTitleEl.textContent = item.snippet.title;
-                }                
-            }
-        })
+                    if (item.snippet.title != null || item.snippet.title != undefined) {
+                        blockTitleEl.textContent ="" //item.snippet.title;
+                    }
+                }
+            })
     });
 
     // movieclipsBtn.addEventListener("click", function() {
